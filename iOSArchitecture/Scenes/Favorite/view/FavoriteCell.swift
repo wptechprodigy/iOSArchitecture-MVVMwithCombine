de5e944717aea71fc2ace02439c52c0a6ca6798d
Waheed Afolabi <waheed.freelancer16@gmail.com>
2022-07-01 01:58:16 +0100

    Setup basic flow for `Favorites`

---
+++ iOSArchitecture/Scenes/Favorite/view/FavoriteCell.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteCell.swift
+//  iOSArchitecture
+//
+//  Created by waheedCodes on 01/07/2022.
+//
+
+import UIKit
+
+class FavoriteCell: UITableViewCell {
+
+    // MARK: - Reuse identifier
+
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
--- iOSArchitecture/Supporting Files/SceneDelegate.swift
+++ iOSArchitecture/Supporting Files/SceneDelegate.swift
@@ -13,13 +13,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     private let appDependencyContainer = AppDependencyContainer()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.windowScene = windowScene
+
+        UITabBar.appearance().tintColor = #colorLiteral(red: 0.1137254902, green: 0.7254901961, blue: 0.3294117647, alpha: 1)
         
         if let window = window {
-            window.rootViewController = appDependencyContainer.makeInitialIntialAppScene()
+            window.rootViewController = appDependencyContainer.makeSignInFlow()
             window.makeKeyAndVisible()
         }
     }
