ad86428aeae4f9ccc09af21bbf2e413ba9d9b491
Waheed Afolabi <waheed.freelancer16@gmail.com>
2022-07-07 13:06:56 +0100

    Created an interface for the models to prepare for sectioning

---
+++ iOSArchitecture/Core/domain/model/DataProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  DataProtocol.swift
+//  iOSArchitecture
+//
+//  Created by waheedCodes on 06/07/2022.
+//
+
+import Foundation
+
+protocol DataProtocol {
+    var artist: String { get set }
+}
